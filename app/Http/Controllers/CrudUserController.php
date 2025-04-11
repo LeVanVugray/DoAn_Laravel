b1275db561e88cb4384faae6ad2fd35f58f1c6e0
GachaBuddies <thanhngoc3009@gmail.com>
2025-04-11 09:41:23 +0700

    Replace to Html, using css, changing CRUD, adding Socialite

---
+++ app/Http/Controllers/CrudUserController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use Session;
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * CRUD User controller
+ */
+class CrudUserController extends Controller
+{
+
+    /**
+     * Login page
+     */
+    public function login()
+    {
+        return view('crud_user.login');
+    }
+    
+
+    // detail_search.blade
+
+
+    //
+    
+    
+
+    /**
+     * User submit form login
+     */
+    public function authUser(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('index')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    /**
+     * Registration page
+     */
+    public function createUser()
+    {
+        return view('crud_user.create');
+    }
+
+    /**
+     * User submit form register
+     */
+    public function postUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+            'phone' => 'required|string|max:15',
+            'address' => 'required|string|max:255',
+        ]);
+    
+        $check = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ]);
+
+        return redirect()->route('login')->withSuccess('You can now login');
+    }
+
+    /**
+     * View user detail page
+     */
+    public function readUser(Request $request) {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.read', ['messi' => $user]);
+    }
+
+    /**
+     * Delete user by id
+     */
+    public function deleteUser(Request $request) {
+        $user_id = $request->get('id');
+        $user = User::destroy($user_id);
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * Form update user page
+     */
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.update', ['user' => $user]);
+    }
+
+    /**
+     * Submit form update user
+     */
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+            'phone' => 'required|string|max:15',
+            'address' => 'required|string|max:255',
+
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ]);
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * List of users
+     */
+    public function listUser()
+    {
+        if(Auth::check()){
+            $users = User::all();
+            $products = Product::all();
+            return view('crud_user.list', ['users' => $users],['products' => $products]);
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'user_id'); // Giả sử mỗi sản phẩm có thuộc tính user_id
+    }
+    /**
+     * Sign out
+     */
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
