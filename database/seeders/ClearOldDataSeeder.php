92cdf9d083681e6e082ad6a119b65571161d06ee
LeVanVugray <23211TT4370@mail.tdc.edu.vn>
2025-04-17 22:05:44 +0700

    Add Do An Laravel

---
+++ database/seeders/ClearOldDataSeeder.php
@@ -0,0 +1,14 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ClearOldDataSeeder extends Seeder
+{
+    public function run()
+    {
+        
+    }
+}
+
