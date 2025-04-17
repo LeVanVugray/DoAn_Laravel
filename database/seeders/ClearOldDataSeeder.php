f45b144c15fcef49d50f52c5b73d21f06bafaecf
LeVanVugray <23211TT4370@mail.tdc.edu.vn>
2025-04-17 22:08:46 +0700

    Tao Du Lieu Cho Bang Va Xoa Bang

--- database/seeders/ClearOldDataSeeder.php
+++ database/seeders/ClearOldDataSeeder.php
@@ -8,7 +8,19 @@ class ClearOldDataSeeder extends Seeder
 {
     public function run()
     {
+        // Tắt các ràng buộc khóa ngoại
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         
+        // Xóa dữ liệu trong các bảng sử dụng query builder
+        DB::table('products')->delete();
+        DB::table('categories')->delete();
+        DB::table('manufactures')->delete();
+        DB::table('users')->delete();
+        DB::table('user')->delete();
+        // Các bảng khác có thể xóa tại đây
+        
+        // Bật lại các ràng buộc khóa ngoại
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
 
